35c758096301bbcc85779d8c05c4f0bddb540e3b
“Kevin <kvnzavalza@hotmail.com>
2026-03-04 18:17:36 -0600

    feat(scanning): add daemon settings module

---
+++ scanning/settings/project/daemon.py
@@ -0,0 +1,13 @@
+import environ
+
+env = environ.FileAwareEnv()
+
+# How often the daemon polls for unprocessed scans (seconds).
+DAEMON_PROCESS_SCANS_INTERVAL = env.int(
+    "DAEMON_PROCESS_SCANS_INTERVAL", default=60
+)
+
+# Seconds before a PROCESSING scan is considered stuck.
+DAEMON_PROCESSING_TIMEOUT = env.int(
+    "DAEMON_PROCESSING_TIMEOUT", default=3600
+)
